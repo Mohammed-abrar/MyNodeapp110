ab70700e90aebf55b81d5e305941b3b90ab620a5
Mohammed Abrar <mohammed.abrar95@gmail.com>
2017-04-28 12:32:10 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ var router = express.Router();
 var uri = mongodb://akshaykumargowdar:Ak$h@y94@mycluster-shard-00-00-rplbd.mongodb.net:27017,mycluster-shard-00-01-rplbd.mongodb.net:27017,mycluster-shard-00-02-rplbd.mongodb.net:27017/MyDatabase?ssl=true&replicaSet=MyCluster-shard-0&authSource=admin ;
 MongoClient.connect(uri, function(err, db) {
   console.log("DB connected");
-  db.close();
 });
 
 var TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
